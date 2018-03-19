55db4b9ed715e4dc584474682e7e6e91166b4660
Arturas Smorgun <arturas@spikerlabs.com>
2018-03-19 20:26:43 +0000

    Add tests for happy path in api

--- src/test/scala/com/spikerlabs/accounts/ApiSpec.scala
+++ src/test/scala/com/spikerlabs/accounts/ApiSpec.scala
@@ -11,8 +11,10 @@ import monix.eval.Task
 import org.scalatest._
 import org.scalatest.Matchers._
 import monix.execution.Scheduler.Implicits.global
-import org.http4s.{Method, Request, Status, Uri}
+import org.http4s.{EntityBody, Method, Request, Status, Uri}
 import io.circe.fs2._
+import fs2.Stream
+import fs2.text.utf8Encode
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
@@ -21,6 +23,33 @@ class ApiSpec extends AsyncFlatSpec {
 
   val storage = InMemoryStorage()
   val api = Api.httpService(Service(storage))
+
+  def parseBody(body: EntityBody[Task]): String = Await.result(body.compile.fold("")(_ + _.toChar).runAsync, 1.second)
+  def makeBody(body: String): EntityBody[Task] = Stream(body).through(utf8Encode)
+
+  it should "transfer the funds" in {
+    val oneAccount = Account().deposit(Money(500))
+    val otherAccount = Account()
+    Await.ready(storage.storeAccount(oneAccount, otherAccount).runAsync, 1.second)
+    val request = Request[Task](
+      Method.POST,
+      Uri.uri("/transfer"),
+      body = makeBody(s"""{"source": {"id": "${oneAccount.id.id.toString}"}, "destination": {"id": "${otherAccount.id.id.toString}"}, "funds": {"amount": 50}}""")
+    )
+
+    api.run(request).value.flatMap { maybeResponse =>
+      assert(maybeResponse.isDefined)
+      assert(maybeResponse.get.status == Status.Ok)
+      storage.findAccount(oneAccount.id).map((maybeResponse.get, _))
+    }.flatMap {
+      case (response, maybeSourceAccount) =>
+        assert(maybeSourceAccount.get.balance == Money(450))
+        storage.findAccount(otherAccount.id).map((response, maybeSourceAccount, _))
+    }.map {
+      case (response, maybeSourceAccount, maybeDestinationAccount) =>
+        assert(maybeDestinationAccount.get.balance == Money(50))
+    }.runAsync
+  }
 
   it should "create an account" in {
     val request = Request[Task](Method.POST, Uri.uri("/create"))
@@ -35,6 +64,31 @@ class ApiSpec extends AsyncFlatSpec {
         val maybeAccount = Await.result(storage.findAccount(response.account).runAsync, 1.second)
         assert(maybeAccount.isDefined)
         assert(maybeAccount.get.id == response.account)
+      }
+      succeed
+    }.runAsync
+  }
+
+  it should "deposit an account" in {
+    val account = Account().deposit(Money(100))
+    Await.ready(storage.storeAccount(account).runAsync, 1.second)
+    val request = Request[Task](
+      Method.POST,
+      Uri.uri("/deposit"),
+      body = makeBody(s"""{"account": {"id": "${account.id.id.toString}"}, "funds": {"amount": 200}}""")
+    )
+    api.run(request).value.map { maybeResponse =>
+      assert(maybeResponse.isDefined)
+      assert(maybeResponse.get.status == Status.Ok)
+      val body = Await.result(maybeResponse.get.body.compile.fold("")(_ + _.toChar).runAsync, 1.second)
+      val eitherResponse = decode[Response.Account](body)
+      assert(eitherResponse.isRight)
+      eitherResponse.foreach { response =>
+        response shouldBe a[Response.Account]
+        val maybeAccount = Await.result(storage.findAccount(response.account).runAsync, 1.second)
+        assert(maybeAccount.isDefined)
+        assert(maybeAccount.get.id == response.account)
+        assert(maybeAccount.get.balance == Money(300))
       }
       succeed
     }.runAsync
